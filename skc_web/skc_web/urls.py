c3635c9b36f7aa869c7b6ec139a245f16055741b
julietteBrt <61454067+julietteBrt@users.noreply.github.com>
2021-01-10 08:38:11 +0100

    Add files via upload

---
+++ skc_web/skc_web/urls.py
@@ -0,0 +1,11 @@
+# default present
+from django.contrib import admin
+from django.urls import path
+from api import views
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.result, name = 'home'),
+]
